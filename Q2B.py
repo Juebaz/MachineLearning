a94f3c94df7e0552e9942e6da3f44df37430d2ec
juebaz <julie.bazerghi@outlook.com>
2021-09-20 10:46:16 -0400

    works git add -A!

--- Q2B.py
+++ Q2B.py
@@ -9,7 +9,11 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import NearestCentroid
+import time
 
+
+TMAX_Q2B = 1.5
+ERRMAX_Q2B = 0.22
 
 
 def make_meshgrid(x, y, h=.02):
@@ -26,101 +30,34 @@ def plot_contours(ax, classificator, xx, yy, **params):
     out = ax.contourf(xx, yy, Z, **params)
     return out
 
-# Durée d'exécution maximale / maximum execution time
-TMAX_Q2B = 1.5
 
-# Erreur maximale attendue / maximum error expected
-ERRMAX_Q2B = 0.22
-
-iris = datasets.load_iris()
-
-pairsOfFeatures = [(i, j) for i in range(4) for j in range(i+1, 4)]
-
-# Dictionnaire pour enregistrer les erreurs selon les classifieurs
-# Dictionary to record errors according to classifiers
-erreurs = collections.defaultdict(list)
-erreurs['Classifieurs'] = []
-
-regularizeParam = 1.0  # SVM regularization parameter
-
-models = (svm.SVC(kernel='linear', C=regularizeParam),
-          svm.LinearSVC(C=regularizeParam, max_iter=10000),
-          svm.SVC(kernel='rbf', gamma=0.7, C=regularizeParam),
-          svm.SVC(kernel='poly', degree=3, gamma='auto', C=regularizeParam))
-
-titles = ('SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel')
-
-# Traiter par paires de mesures avec réentraînement
-# Process with pairs of measures with retraining
-for (f1, f2) in pairsOfFeatures:
-    f1_name = iris.feature_names[f1]
-    f2_name = iris.feature_names[f2]
-    
-     # *** TODO Q2B ***
-    # Créez un jeu de données contenant seulement f1 et f2
-    # Create a dataset with only f1 and f2
-    X = iris.data[:, [f1,f2]]
-    y = iris.target
-    
-   
-    # Initialisez différents classifieurs dans la liste nommée 'classifieurs'
-    # Initialize various classifiers in the list named 'classifiers'
-
-
-    trainModels = (classificator.fit(X, y) for classificator in models)
-    
-
-    fig, sub = pyplot.subplots(2, 2)
-    pyplot.subplots_adjust(wspace=0.4, hspace=0.4)
-
-    X0, X1 = X[:, 0], X[:, 1]
-    xx, yy = make_meshgrid(X0, X1)
-
-    for classificator, title, ax in zip(trainModels, titles, sub.flatten()):
-        plot_contours(ax, classificator, xx, yy,
-                    cmap=pyplot.cm.coolwarm, alpha=0.8)
-        ax.scatter(X0, X1, c=y, cmap=pyplot.cm.coolwarm, s=20, edgecolors='k')
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xlabel('Sepal length')
-        ax.set_ylabel('Sepal width')
-        ax.set_xticks(())
-        ax.set_yticks(())
-        ax.set_title(title)
-
-    pyplot.show()
-
-
-
-
-def plotClassicationGraphForFeatures(f1,f2,data): 
+def plotClassicationGraphForFeatures(f1, f2, data):
     regularizeParam = 1.0  # SVM regularization parameter
 
     models = (svm.SVC(kernel='linear', C=regularizeParam),
-          svm.LinearSVC(C=regularizeParam, max_iter=10000),
-          svm.SVC(kernel='rbf', gamma=0.7, C=regularizeParam),
-          svm.SVC(kernel='poly', degree=3, gamma='auto', C=regularizeParam))
+              svm.LinearSVC(C=regularizeParam, max_iter=10000),
+              svm.SVC(kernel='rbf', gamma=0.7, C=regularizeParam),
+              svm.SVC(kernel='poly', degree=3, gamma='auto', C=regularizeParam))
 
     titles = ('SVC with linear kernel',
-          'LinearSVC (linear kernel)',
-          'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel')
+              'LinearSVC (linear kernel)',
+              'SVC with RBF kernel',
+              'SVC with polynomial (degree 3) kernel')
 
-    X = data.data[:, [f1,f2]]
+    X = data.data[:, [f1, f2]]
     y = data.target
 
     X0, X1 = X[:, 0], X[:, 1]
     xx, yy = make_meshgrid(X0, X1)
 
     fig, subfigs = pyplot.subplots(2, 2, sharex='all', sharey='all',
-                                  tight_layout=True)
+                                   tight_layout=True)
+    fig.suptitle(data.feature_names[f1]+' vs ' +
+                 data.feature_names[f2], fontsize=12)
 
-    for classificator,subfig,title in zip(models, subfigs.reshape(-1),titles):
+    for classificator, subfig, title in zip(models, subfigs.reshape(-1), titles):
         trainModels = classificator.fit(X, y)
-        
+
         plot_contours(subfig, classificator, xx, yy,
                       cmap=pyplot.cm.coolwarm, alpha=0.8)
         subfig.scatter(X0, X1, c=y, cmap=pyplot.cm.coolwarm,
@@ -134,3 +71,33 @@ def plotClassicationGraphForFeatures(f1,f2,data):
         subfig.set_title(title)
 
     return fig
+
+
+def main():
+    iris = datasets.load_iris()
+
+    pairsOfFeatures = [(i, j) for i in range(4) for j in range(i+1, 4)]
+
+    regularizeParam = 1.0  # SVM regularization parameter
+
+    models = (svm.SVC(kernel='linear', C=regularizeParam),
+              svm.LinearSVC(C=regularizeParam, max_iter=10000),
+              svm.SVC(kernel='rbf', gamma=0.7, C=regularizeParam),
+              svm.SVC(kernel='poly', degree=3, gamma='auto', C=regularizeParam))
+
+    titles = ('SVC with linear kernel',
+              'LinearSVC (linear kernel)',
+              'SVC with RBF kernel',
+              'SVC with polynomial (degree 3) kernel')
+
+    for (f1, f2) in pairsOfFeatures:
+        f1_name = iris.feature_names[f1]
+        f2_name = iris.feature_names[f2]
+        regularizeParam = 1.0  # SVM regularization parameter
+
+        plotClassicationGraphForFeatures(f1, f2, iris)
+
+    pyplot.show()
+
+
+main()
