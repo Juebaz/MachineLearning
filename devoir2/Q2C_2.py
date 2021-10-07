a3dd447d52a05543d48842d3f74f638f7999beb1
juebaz <julie.bazerghi@outlook.com>
2021-10-07 15:42:54 -0400

    working

--- devoir2/Q2C_2.py
+++ devoir2/Q2C_2.py
@@ -16,6 +16,21 @@ import numpy
 import pandas
 from Q2B import DiscriminantLineaire
 
+_times = []
+
+
+def checkTime(maxduration, question):
+    duration = _times[-1] - _times[-2]
+    if duration > maxduration:
+        print("[ATTENTION] Votre code pour la question {0} met trop de temps à s'exécuter! ".format(question) +
+              "Le temps maximum permis est de {0:.4f} secondes, mais votre code a requis {1:.4f} secondes! ".format(maxduration, duration) +
+              "Assurez-vous que vous ne faites pas d'appels bloquants (par exemple à show()) dans cette boucle!")
+
+
+# Durée d'exécution maximale / maximum execution time
+TMAX_Q2C = 10.0
+_times.append(time.time())
+
 
 def make_meshgrid(x, y, h=.02):
     x_min, x_max = x.min() - 1, x.max() + 1
@@ -32,9 +47,7 @@ def plot_contours(ax, classificator, xx, yy, **params):
     return out
 
 
-
 # Durée d'exécution maximale / maximum execution time
-
 
 # Problème à 2 classes / 2-class problem
 X, y = make_classification(n_features=2,
@@ -42,21 +55,6 @@ X, y = make_classification(n_features=2,
                            n_informative=2,
                            n_clusters_per_class=1)
 
-# *** TODO Q2C ***
-# Créez ici une grille permettant d'afficher les régions de
-# décision pour chaque classifieur
-# Indice : numpy.meshgrid pourrait vous être utile ici
-# N'utilisez pas un pas trop petit!
-
-
-
-# Create a grid here to display the decision regions for each
-# decision regions for each classifier
-# Tip: numpy.meshgrid might be useful here
-# Don't use a too small step size!
-
-# Entraînez le discriminant linéaire implémenté
-# Train the implemented linear discriminant
 discriminant = DiscriminantLineaire()
 
 discriminant.fit(X, y)
@@ -65,7 +63,7 @@ ypred = discriminant.predict(X)
 # Testez la performance du discriminant linéaire pour le problème
 # Test the performance of the linear discriminant for the problem
 
-score = discriminant.score(X,y)
+score = discriminant.score(X, y)
 
 # Création de la figure à afficher
 # Create the figure to display
@@ -75,22 +73,20 @@ ax = fig.add_subplot(111)
 X0, X1 = X[:, 0], X[:, 1]
 xx, yy = make_meshgrid(X0, X1)
 
-plot_contours(ax, discriminant, xx, yy,
-              cmap=pyplot.cm.coolwarm, alpha=0.8)
-# Utilisez la grille que vous avez créée plus haut
-# pour afficher les régions de décision, de même
-# que les points colorés selon leur vraie classe
-# N'oubliez pas la légende !
-# Use the grid you created above
-# to display the decision regions, as well as
-# coloured points according to their true class
-# Don't forget the legend!
 
-ax.set_title("Title")   # À modifier / to be modified
-ax.set_xlabel("X Axis")  # À modifier / to be modified
-ax.set_ylabel("Y Axis")  # À modifier / to be modified
-#ax.contourf() # À compléter / to be completed
+# À modifier / to be modified
+ax.set_title("Classification with Linear Discriminant and Gradient Descent")
+ax.set_xlabel("feature 1")
+ax.set_ylabel("feature 2")
+
+plot_contours(ax, discriminant, xx, yy, cmap=pyplot.cm.coolwarm, alpha=0.8)
 ax.scatter(X[:, 0], X[:, 1], c=y, cmap=pyplot.cm.coolwarm,
            s=20)
+ax.text(1, 1, 'Score:'+str(score), fontsize=15,
+        bbox={'facecolor': 'green', 'alpha': 0.5, 'pad': 10})
+
+
+_times.append(time.time())
+checkTime(TMAX_Q2C, "Q2C")
+
 pyplot.show()
-# ******
